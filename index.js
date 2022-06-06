d5654f49306aca51b2422bbb9c53ff559d98b30c
NouhRastanawi <noahrastdark@gmail.com>
2022-06-06 15:17:58 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let len = Math.floor(word.length / 2);  // Floor for the half the word length.
+  for (let i = 0; i < len; i++)           // looping through the first half the word.
+    if (word[i] !== word[word.length - i - 1]) return false; 
+     // when a letter with its corresponding letter from the other half doesn't match, return false (not Palindrome).
+  return true;                            //  otherwise the word is palindrome (true).
 }
 
-/* 
-  Add your pseudocode here
-*/
+console.log("check: ", isPalindrome("roor"));
 
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
